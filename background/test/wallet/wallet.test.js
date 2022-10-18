bef9f0b2cb710c43c75a920ee8fb04b06a9d1934
Akamig <akamig@posteo.net>
2022-10-18 15:09:54 +0900

    Remove ECCrypto test

--- background/test/wallet/wallet.test.js
+++ background/test/wallet/wallet.test.js
@@ -2,10 +2,7 @@ import Wallet from "@/wallet/wallet"
 import Mock from "../mock"
 import {ENCRYPTED_WALLET} from "@/constants/constants"
 import crypto from "crypto";
-import {sign} from "eccrypto";
-import keccak256 from "keccak256";
 const ethers = require("ethers")
-const eccrypto = require("eccrypto")
 
 Mock.mockGraphql()
 Mock.mockStorage()
@@ -45,21 +42,11 @@ describe("wallet.js", () => {
         expect(await wallet.validateSignature(signature, data, address)).toBeTruthy()
     })
 
-    test('eccrypto sign', async () => {
-        let privateKey = '0x5b723d0f796c15951cfcb0134351f59279c0e95fbdc8496bd71213d20ee4f66a'
-        console.log('pk', privateKey)
-        let message = crypto.createHash('sha256').update('hello', 'base64').digest();
-        console.log('message', ethers.utils.hexlify(message))
-        eccrypto.sign(Buffer.from(ethers.utils.arrayify(privateKey, {allowMissingPrefix: true})), message).then(sig => {
-            console.log(ethers.utils.hexlify(sig))
-        })
-
-    })
 
     test('Mock send NCG test', async () => {
         let receiver = '0x48BD02A8ADe581A55743646c8880F307F2e8e79D'
         await expect(async () => await wallet.sendNCG(address, 'other', 100, 1)).rejects.toEqual("Invalid Nonce")
-        let result = await wallet.sendNCG(address, receiver, 100, await wallet.nextNonce())
+        let result = await wallet.sendNCG(address, receiver, 100, await wallet.nextNonce(address))
 
         expect(result['status']).toBe('STAGING')
         expect(result['signer']).toBe(address)
